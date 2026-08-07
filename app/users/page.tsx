250e54b4e3638f01dddb02d2e0c30af4f27ac575
golukumarsharma <sharmagolukumar236@gmail.com>
2026-08-07 16:23:53 +0530

    Fix users page

---
+++ app/users/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function UsersPage() {
+  return (
+    <div
+      style={{
+        padding: "30px",
+      }}
+    >
+      <h1>👥 Users</h1>
+
+      <p>
+        Users page is under development.
+      </p>
+    </div>
+  );
+}
